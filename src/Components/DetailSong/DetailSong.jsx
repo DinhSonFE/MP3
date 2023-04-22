4fbc557edf6b30757130a6b7bdfc0ad093ab16b2
Nguyen Dinh Son <dinhson24072004@gmail.com>
2023-04-22 20:52:30 +0700

    fix responsive

--- src/Components/DetailSong/DetailSong.jsx
+++ src/Components/DetailSong/DetailSong.jsx
@@ -4,7 +4,7 @@ import "./DetailSong.css";
 const DetailSong = ({ detailSong }) => {
   const { playingSong } = useContext(context);
   return (
-    <div className="col-span-1 items-center flex justify-between flex-col md:border-r md:border-white h-content-height">
+    <div className="col-span-1 items-center flex justify-between flex-col md:border-r md:border-white max-h-screen">
       <div className="">
         <h1 className="text-gray-400 font-extrabold text-xs">NOW PLAYING</h1>
         <span className="font-extrabold text-2xl text-white">
@@ -12,7 +12,7 @@ const DetailSong = ({ detailSong }) => {
         </span>
       </div>
       <div
-        className={`max-w-72 max-h-72 w-72 h-72  flex justify-center items-center relative before:content-[''] before:absolute before:w-12 before:h-12 before:bg-white before:rounded-full before:border-2 before:border-black rounded-full border-2 border-white overflow-hidden ${
+        className={`max-w-64 max-h-64 md:max-w-72 md:max-h-72 md:w-72 md:h-72  flex justify-center items-center relative before:content-[''] before:absolute before:w-12 before:h-12 before:bg-white before:rounded-full before:border-2 before:border-black rounded-full border-2 border-white overflow-hidden ${
           playingSong ? "animation-music" : ""
         }`}
       >
